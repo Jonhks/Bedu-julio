6c5a96a19b3ebef52d726af4e4fd6eb712e08602
Jonhks <jonhgx@gmail.com>
2024-07-08 22:04:00 -0600

    Update/ create repo

---
+++ clase-01/app.js
@@ -0,0 +1,107 @@
+// // ES5
+// // var nombre_completo = "Jonathan Parra";
+// var nombreCompleto = "Jonathan Parra";
+// var nombreCompleto = "4";
+// var nombreCompleto = "true";
+// var nombreCompleto = "true";
+// var nombreCompleto = `true`;
+
+// var edad = -38.3423423;
+
+// var mexicano = false;
+
+// var persona = {
+//   nombre: "Jonh",
+//   edad: 38,
+// };
+
+// console.log(persona.nombre);
+// console.log(persona["edad"]);
+// //                       1      2         3
+// var listaUsuuarios = ["Pedro", "Cris", "Jonh"];
+// //                      0        1        2
+
+// // console.log(listaUsuuarios[1]);
+
+// var nombre = "Usuario";
+
+// console.log(nombre);
+
+// // fundionalidad del login
+// // var login = () => {};
+
+// // nombre = "jonh";
+
+// var nombre = null;
+
+// nombre === '' ? "Usuario" : "Jonh";
+
+// console.log("He said `Hello` and left.");
+
+// var nombre = ""; // jonh
+
+// var nombre = "Cris"; //
+
+// var nombre = "Isra";
+
+// var nombre = Infinity;
+
+// console.log(typeof nombre);
+
+// var nombre = "Jonh";
+// var apellido = "Parra";
+
+// console.log(nombre + apellido);
+
+// var num1 = "2";
+// var num2 = 4;
+
+// console.log(num1 + num2);
+
+// var numeroUsuario = prompt("Ingresa un número");
+
+// console.log(numeroUsuario + 5);
+
+// console.log(typeof (123 + ""));
+// console.log(Number("-10"));
+
+// var nombre = "Jonh"; // jonh
+
+// var nombre = true; //
+
+// nombre = 2;
+
+// if (true) {
+//   var nombre = "pepe";
+// }
+
+// console.log(nombre);
+
+// ? let y const
+// const nombre = "Jonh";
+
+// nombre = true;
+// if (true) {
+//   const nombre = "pepe";
+//   console.log(nombre);
+// }
+
+// const login = () => {
+//   return "Estamos logueados";
+// };
+
+// console.log(login());
+
+// let nombre = "Usuario";
+
+// nombre = "Jonh";
+
+// if (true) {
+//   let nombre = "pepe";
+//   console.log(nombre);
+// }
+// console.log(nombre);
+
+// let number = 5;
+
+// console.log(number++);
---
+++ clase-02/app.js
@@ -0,0 +1,114 @@
+// const edadUsuario = Number(prompt("¿Cuál es tu edad?"));
+
+// // console.log(isNaN(edadUsuario));
+
+// if (typeof edadUsuario !== "number" || isNaN(edadUsuario)) {
+//   alert("El valor ingresado debe ser un número");
+//   window.location.reload();
+// } else {
+//   if (edadUsuario >= 18) {
+//     document.write(
+//       "Bienvenido a nuestra pag," + " Tu edad es :" + edadUsuario + " años"
+//     );
+//   } else {
+//     document.write(
+//       "No te podemos vender nada, regresa cuando seas mayor de edad!! " +
+//         " Tu edad es :" +
+//         edadUsuario +
+//         " años"
+//     );
+//   }
+// }
+
+// const response = "9";
+
+// if (typeof response === "string") {
+//   document.write("El valor es valido");
+// } else {
+//   document.write("Solo se aceptan valores de tipo texto");
+// }
+
+// const time = 21;
+// let saludo;
+
+// if (time >= 0 && time < 12) {
+//   saludo = "Buenos días";
+// } else if (time >= 12 && time < 20) {
+//   saludo = "Buenas tardes";
+// } else if (time >= 20 && time < 24) {
+//   saludo = "Buenas Noches";
+// } else {
+//   saludo = "Hora no valida";
+// }
+
+// console.log(saludo);
+
+// const dia = 0;
+// let msj;
+
+// switch (dia) {
+//   case 0:
+//     msj = "Domingo";
+//     break;
+//   case 1:
+//     msj = "Lunes";
+//     break;
+//   case 2:
+//     msj = "Martes";
+//     break;
+//   case 3:
+//     msj = "Miercoles";
+//     break;
+//   case 4:
+//     msj = "Jueves";
+//     break;
+//   case 5:
+//     msj = "Viernes";
+//     break;
+//   case 6:
+//     mjs = "Sabado";
+//     break;
+//   default:
+//     msj = "Día invalido";
+//     break;
+// }
+
+// document.write(msj);
+
+// const edad = 8;
+
+// const msj = edad >= 18 ? "Bienvenido a nuesta pag" : "No te pondemos vender";
+
+// document.write(msj);
+
+// for (let i = 0; i <= 1000; i++) {
+// document.write("Número " +  + "<br>");
+// console.log("Hola mundo");
+// }
+
+// ejecuta lo global
+
+// i   log
+// 0    0
+// 1    1
+// 2    2
+// 3    3
+// 4    4
+// 5    5
+// 6
+
+// numero % 2 === 0;
+
+for (let numero = 0; numero <= 100; numero++) {
+  // if (numero % 2 === 0) {
+  //   document.write("Número par " + numero + "<br>");
+  // } else {
+  //   document.write(`Número Impar ${numero} <br>`);
+  // }
+  // numero % 2 === 0
+  //   ? document.write("Número par " + numero + "<br>")
+  //   : document.write(`Número Impar ${numero} <br>`);
+
+  numero % 2 === 0 && document.write("Número par " + numero + "<br>");
+  // numero % 2 === 0 ? document.write("Número par " + numero + "<br>") : "";
+}
